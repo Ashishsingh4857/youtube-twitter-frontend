1ae4ca166bea54071c50482cefd3407fa98a27bb
Ashish singh <ashishsingh4857@gmail.com>
2025-11-03 01:56:56 +0530

    feat: Add video management functionality with async actions

--- src/store/slices/videoSlice.js
+++ src/store/slices/videoSlice.js
@@ -1,4 +1,120 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../../helpers/axiosInstance.js";
+import { toast } from "react-toastify";
+import { BASE_URL } from "../../constants.js";
+
+export const publishAvideo = createAsyncThunk("publishAvideo", async (data) => {
+  const formData = new FormData();
+  formData.append("title", data.title);
+  formData.append("description", data.description);
+  formData.append("videoFile", data.videoFile[0]);
+  formData.append("thumbnail", data.thumbnail[0]);
+
+  try {
+    const response = await axiosInstance.post("/videos", formData);
+    toast.success(response?.data?.message);
+    return response.data.data;
+  } catch (error) {
+    toast.error(error?.response?.data?.error);
+    throw error;
+  }
+});
+export const getAllVideos = createAsyncThunk(
+  "getAllVideos",
+  async ({ userId, sortBy, sortType, query, page, limit }) => {
+    try {
+      //video url
+      const url = new URL(`${BASE_URL}/videos`);
+
+      if (userId) url.searchParams.set("userId", userId);
+      if (query) url.searchParams.set("query", query);
+      if (page) url.searchParams.set("page", page);
+      if (limit) url.searchParams.set("limit", limit);
+      if (sortBy && sortType) {
+        url.searchParams.set("sortBy", sortBy);
+        url.searchParams.set("sortType", sortType);
+      }
+
+      const response = await axiosInstance.get(url);
+
+      return response.data.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
+export const updateAVideo = createAsyncThunk(
+  "updateAVideo",
+  async ({ videoId, data }) => {
+    const formData = new FormData();
+    if (data.title) {
+      formData.append("title", data.title);
+    }
+    if (data.description) {
+      formData.append("description", data.description);
+    }
+    if (data.thumbnail) {
+      formData.append("thumbnail", data.thumbnail[0]);
+    }
+
+    try {
+      const response = await axiosInstance.patch(
+        `/videos/${videoId}`,
+        formData
+      );
+      toast.success(response?.data?.message);
+      return response.data?.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
+export const deleteAVideo = createAsyncThunk(
+  "deleteAVideo",
+  async (videoId) => {
+    try {
+      const response = await axiosInstance.delete(`/videos/${videoId}`);
+      toast.success(response?.data?.message);
+      return response.data?.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
+export const getVideoById = createAsyncThunk(
+  "getVideoById",
+  async ({ videoId }) => {
+    try {
+      const response = await axiosInstance.get(`/videos/${videoId}`);
+      return response.data?.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
+
+export const togglePublishStatus = createAsyncThunk(
+  "togglePublishStatus",
+  async (videoId) => {
+    try {
+      const response = await axiosInstance.patch(
+        `/videos/toggle/publish/${videoId}`
+      );
+      toast.success(response.data?.message);
+      return response.data?.data?.isPublished;
+    } catch (error) {
+      toast.error(error?.response?.data?.error);
+      throw error;
+    }
+  }
+);
 
 //initialState
 const initialState = {
@@ -28,6 +144,48 @@ const videoSlice = createSlice({
     makeVideosNull: (state) => {
       state.videos.docs = [];
     },
+  },
+  // builder callback function that allows you to  define additional reducers
+  //add case for loading states
+  extraReducers: (builder) => {
+    builder.addCase(getAllVideos.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getAllVideos.fulfilled, (state, action) => {
+      state.loading = false;
+      state.videos.docs = [...state.videos.docs, ...action.payload.docs];
+      state.videos.hasNextPage = action.payload.hasNextPage;
+    });
+    builder.addCase(publishAvideo.pending, (state) => {
+      state.uploading = true;
+    });
+    builder.addCase(publishAvideo.fulfilled, (state) => {
+      state.uploading = false;
+      state.uploaded = true;
+    });
+    builder.addCase(updateAVideo.pending, (state) => {
+      state.uploading = true;
+    });
+    builder.addCase(updateAVideo.fulfilled, (state) => {
+      state.uploading = false;
+      state.uploaded = true;
+    });
+    builder.addCase(deleteAVideo.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(deleteAVideo.fulfilled, (state) => {
+      state.loading = false;
+    });
+    builder.addCase(getVideoById.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getVideoById.fulfilled, (state, action) => {
+      state.loading = false;
+      state.video = action.payload;
+    });
+    builder.addCase(togglePublishStatus.fulfilled, (state) => {
+      state.publishToggled = !state.publishToggled;
+    });
   },
 });
 
