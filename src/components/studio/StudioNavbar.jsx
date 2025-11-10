8ccb10f3552a6287c096ff22586bf97cf029b0ad
Ashish singh <ashishsingh4857@gmail.com>
2025-11-11 00:12:20 +0530

    feat: implement avatar  dropdown  Menu functionality in studio navbar and enhance ui

--- src/components/studio/StudioNavbar.jsx
+++ src/components/studio/StudioNavbar.jsx
@@ -5,35 +5,65 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { BsQuestionCircle } from "react-icons/bs";
 import { HiOutlinePlus } from "react-icons/hi";
 import { FiMenu } from "react-icons/fi";
-import { Logo, Input, Button } from "../index.js";
-import { useSelector } from "react-redux";
+import { Logo, Input, Button, DropdownMenu } from "../index.js";
+import { useSelector, useDispatch } from "react-redux";
+import ClickAwayListener from "react-click-away-listener";
+import { userLogout } from "../../store/slices/authSlice.js";
+import { useNavigate } from "react-router-dom";
+// icons
+import { AiOutlineSetting, AiOutlineLogout } from "react-icons/ai";
+import { FaRegMoon } from "react-icons/fa";
+import { PiVideoDuotone } from "react-icons/pi";
 
 const StudioNavbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  //toggle  search for small screen
   const [showSearch, setShowSearch] = useState(false);
   const { register, handleSubmit, reset } = useForm();
+  // profile dropdown menu toggle
+  const [isStudioProfileDropdownOpen, setIsStudioProfileDropdownOpen] =
+    useState(false);
 
+  // search handler
   const onSubmit = (data) => {
     console.log("Searching for:", data.searchQuery);
     reset();
   };
+
+  //fetching logged in user data from store
   const { userData } = useSelector((state) => state.auth);
   const { avatar, username, fullName } = userData || {};
-  console.log(userData);
 
   // fallback avatar image
   const DEFAULT_AVATAR_URL =
     "https://res.cloudinary.com/ashishsingh4857/image/upload/v1762338092/rhg4ffc6zlc7txxofok2.jpg";
 
+  // avatar dropdown menu  options and icons
+  const profileOptions = [
+    { text: "Sign out", onClick: () => dispatch(userLogout()) },
+    { text: "Main-Menu", onClick: () => navigate("/") },
+    { text: "Appearance: Dark" },
+    { text: "Switch account" },
+  ];
+
+  const icons = {
+    "Sign out": <AiOutlineLogout size={18} />,
+    "Main-Menu": <PiVideoDuotone size={18} />,
+    "Appearance: Dark": <FaRegMoon size={18} />,
+    "Switch account": <AiOutlineSetting size={18} />,
+  };
+
   return (
-    <nav className="bg-gray-900 text-white p-4 flex items-center justify-between shadow-md z-10">
+    <nav className="bg-gray-900 w-full text-white py-3 pr-6 pl-4 flex items-center justify-between shadow-md z-10 fixed top-0">
       <div className="flex items-center">
         {/* sidebar toggle button */}
         <Button className="mr-4 bg-gray-700 p-2 rounded-full hover:bg-gray-600 cursor-pointer">
           <FiMenu size={20} />
         </Button>
         <div className="flex items-center">
-          <Logo text="" />
-          <span className="text-xl font-bold">Studio</span>
+          <Logo text={false} />
+          <span className="text-lg md:text-xl font-bold">Studio</span>
         </div>
       </div>
 
@@ -54,7 +84,7 @@ const StudioNavbar = () => {
 
       {/* search bar for small screen devices*/}
       <ClickAwayListener onClickAway={() => setShowSearch(false)}>
-        <div className="lg:hidden">
+        <div className="lg:hidden ">
           {!showSearch && (
             <button onClick={() => setShowSearch(true)}>
               <AiOutlineSearch size={24} />
@@ -63,7 +93,7 @@ const StudioNavbar = () => {
           {showSearch && (
             <form
               onSubmit={handleSubmit(onSubmit)}
-              className="absolute top-16 left-0 right-0 bg-gray-900 p-4 lg:hidden"
+              className="absolute top-14 left-0 right-0 bg-gray-900 p-4 lg:hidden"
             >
               <Input
                 type="text"
@@ -77,22 +107,47 @@ const StudioNavbar = () => {
       </ClickAwayListener>
 
       <div className="flex items-center space-x-4">
-        <button>
+        <button className="hidden md:block">
           <BsQuestionCircle size={20} />
         </button>
         <button>
           <IoMdNotificationsOutline size={20} />
         </button>
         {/* upload videos*/}
-        <button>
+        <Button className="text-sm  flex items-center md:p-2 rounded-full  hover:bg-gray-600">
           <HiOutlinePlus size={20} /> Create
-        </button>
-        {/* avatar */}
-        <img
-          src={avatar?.url || avatar || DEFAULT_AVATAR_URL}
-          alt="User Avatar"
-          className="w-8 h-8 rounded-full"
-        />
+        </Button>
+        {/* avatar and avatar dropdown */}
+        {userData && (
+          <div className="relative">
+            <img
+              src={avatar?.url || avatar || DEFAULT_AVATAR_URL}
+              alt="avatar"
+              className="w-8 h-8 rounded-full sm:block cursor-pointer"
+              onClick={() =>
+                setIsStudioProfileDropdownOpen(!isStudioProfileDropdownOpen)
+              }
+            />
+            {isStudioProfileDropdownOpen && (
+              <ClickAwayListener
+                onClickAway={() =>
+                  setIsStudioProfileDropdownOpen(!isStudioProfileDropdownOpen)
+                }
+              >
+                <div onClick={(e) => e.stopPropagation()}>
+                  <DropdownMenu
+                    options={profileOptions}
+                    icons={icons}
+                    avatar={userData.avatar?.url}
+                    username={userData.username}
+                    fullName={userData.fullName}
+                    setIsActive={setIsStudioProfileDropdownOpen}
+                  />
+                </div>
+              </ClickAwayListener>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   );
