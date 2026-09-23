10439f72e68633b13a8df41c242eaddbbeb50473
Ashish singh <ashishsingh4857@gmail.com>
2026-09-23 16:13:21 +0530

    fix(layout): add top padding and dynamic left margin for fixed navbar/sidebar
    - Add pt-[56px] to offset fixed navbar
    - Add md:ml-60 / md:ml-[72px] based on sidebar isOpen state for smooth content shift

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Navbar from "../Header/Navbar.jsx";
 import Sidebar from "../Header/Sidebar.jsx";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Layout() {
+  //toggle sidebar
+  const { isOpen } = useSelector((state) => state.global.sidebar);
+
   return (
     <>
       <Navbar />
-      <div className="sm:flex flex-none">
+      <div className="flex pt-[56px] min-h-screen bg-gray-950">
         <Sidebar />
-        <div className="sm:flex-1">
+        {/* content shifts according to sidebar width */}
+        <div
+          className={`flex-1 transition-all duration-300 ease-in-out ${isOpen ? "md:ml-60" : "md:ml-[72px]"}`}
+        >
           <Outlet />
         </div>
       </div>
